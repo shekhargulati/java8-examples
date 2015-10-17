5798d3012cfa1d4b140b26679b1c45ac48cfc5d1
Shekhar Gulati <shekhargulati84@gmail.com>
2015-10-17 13:21:03 +0530

    Sliding example no need of SimpleEntry

--- programs/src/main/java/io/shekhar/java8/programs/sliding/Sliding.java
+++ programs/src/main/java/io/shekhar/java8/programs/sliding/Sliding.java
@@ -1,6 +1,5 @@
 package io.shekhar.java8.programs.sliding;
 
-import java.util.AbstractMap.SimpleEntry;
 import java.util.stream.IntStream;
 
 /**
@@ -17,8 +16,7 @@ public class Sliding {
     public static void main(String[] args) {
         final String input = "abcdef";
         IntStream.range(0, input.length())
-                .mapToObj(index -> new SimpleEntry<>(String.valueOf(input.charAt(index)), index))
-                .map(entry -> input.substring(0, entry.getValue() + 1))
+                .mapToObj(index -> input.substring(0, index + 1))
                 .forEach(System.out::println);
     }
 }
